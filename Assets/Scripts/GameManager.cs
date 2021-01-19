c5fdcdfc5d818db4dc40be4392e5668bcb8597d3
DePacifier <milkybekele@gmail.com>
2021-01-19 12:41:50 +0300

    GameManage Implemented
    A GameManager that dictates the gameover condition implemented with script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GoalScript blue, green, red, orange;
+    private bool isGameOver = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;
+    }
+
+    private void OnGUI()
+    {
+        if (isGameOver)
+        {
+            Rect rect = new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100);
+            GUI.Box(rect, "Game Over");
+            Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 30, 60, 60);
+            GUI.Label(rect2, "Good Job");
+        }
+    }
+}
