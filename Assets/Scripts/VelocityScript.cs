7c3559b2cbb69544988c0f18c66194aed841ce0b
DePacifier <milkybekele@gmail.com>
2021-01-19 11:46:32 +0300

    Chaos Ball Created
    Chaos ball created with its material and script

---
+++ Assets/Scripts/VelocityScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityScript : MonoBehaviour
+{
+
+    public float startSpeed = 50f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(startSpeed,0,startSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
