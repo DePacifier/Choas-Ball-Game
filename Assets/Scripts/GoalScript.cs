d9e32f55654acad9ec072bc72ef04bc7c4df307c
DePacifier <milkybekele@gmail.com>
2021-01-19 12:11:18 +0300

    Goals Implemented
    Goals from the game balls created with lights and collision detection script

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+
+    public bool isSolved = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject collidedWith = other.gameObject;
+        if (gameObject.tag == collidedWith.tag)
+        {
+            isSolved = true;
+            GetComponent<Light>().enabled = false;
+            Destroy(collidedWith);
+        }
+    }
+}
